0252607be7f96dc7dacbed7729b6653254ed42d4
010bnu <2797375907@qq.com>
2019-03-08 23:56:43 +0800

    Update readme.py

--- readme.py
+++ readme.py
@@ -1,7 +1,1 @@
-import pandas as pd
-
-df=pd.read_csv('score.csv',delimiter=' ')
-# print(df['专业代码'])
-print(df[df['专业代码']=='085212'])
-df[df['专业代码']=='085212'].to_csv('085212.csv',index=False,encoding='gbk')
-how to use git
+利用scrapy爬取大众点评
