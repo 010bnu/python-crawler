87ad0bf57aad580ca87a02e55e157b2626b021d3
thomas young <2797375907@qq.com>
2019-03-08 22:43:55 +0800

    begin

---
+++ readme.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df=pd.read_csv('score.csv',delimiter=' ')
+# print(df['专业代码'])
+print(df[df['专业代码']=='085212'])
+df[df['专业代码']=='085212'].to_csv('085212.csv',index=False,encoding='gbk')
