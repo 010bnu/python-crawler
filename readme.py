4279f22f0db4bd4e9ef824d1aeda1e6ac7d8ab97
thomas young <2797375907@qq.com>
2019-03-08 22:47:23 +0800

    second

--- readme.py
+++ readme.py
@@ -4,3 +4,4 @@ df=pd.read_csv('score.csv',delimiter=' ')
 # print(df['专业代码'])
 print(df[df['专业代码']=='085212'])
 df[df['专业代码']=='085212'].to_csv('085212.csv',index=False,encoding='gbk')
+how to use git
